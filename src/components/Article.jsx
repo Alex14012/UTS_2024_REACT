dd4569c69c1eb4655724afa16470919437e6ad08
Alex_DGL112 <legendgg211@gmail.com>
2024-09-23 14:02:44 +0700

    hope this work again

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,15 +1,15 @@
 function Article({titleArticle, image, newsArticle}){
     return(
         <>
-        <div className="flex flex-col h-screen w-screen items-center justify-start font-Arimo">
+        <div className="flex flex-col h-full w-full items-center justify-start font-Arimo">
             <div>
                 <h1 className="text-5xl text-center pb-3 pt-3 text-white">{titleArticle}</h1>
             </div>
             <div>
                 <img src={image} alt="" className="w-[950px]" />
             </div>
-            <div className="bg-white content-center text-center h-full w-full">
-                <p className="pt-8 pb-7 text-lg">{newsArticle}</p>
+            <div className="bg-white content-center text-justify w-full">
+                <p className="pt-8 pb-7 pl-14 pr-14 text-lg">{newsArticle}</p>
             </div>
         </div>
         </>
